866a306c430bdcbd0460783f37417446c1f209c1
Alexander <54470124+AlexanderLind98@users.noreply.github.com>
2024-03-11 14:15:46 -0700

    Camera view per. scene done

--- Assets/Scripts/AutoInit.cs
+++ Assets/Scripts/AutoInit.cs
@@ -6,10 +6,18 @@ public class AutoInit : MonoBehaviour
     //The interaction manager needs to know what the sandbox is, but now we cannot use hard-referencing anymore, so it has to be done from here.
 
     public GameObject scenarioSandbox;
+    public Vector3 defaultCameraPos = new Vector3(0.0f,19.4532013f,-32.7898941f);
+    public Vector3 defaultCameraRot = new Vector3(38.250103f,0.0f,0.0f);
     
     //Set the sandbox to this levels sandbox
     void Start()
     {
         InteractionManager.Instance.sandbox = scenarioSandbox;
+
+        //Setup default camera position
+        // if (!Camera.main) return;
+        
+        Camera.main.transform.position = defaultCameraPos;
+        Camera.main.transform.rotation = Quaternion.Euler(defaultCameraRot);
     }
 }
